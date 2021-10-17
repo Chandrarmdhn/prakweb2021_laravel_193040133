fe7b4a5cf68b0f0ca0b610edcb4442da33f5d4db
unknown <e3000yoisantuy@gmail.com>
2021-10-17 11:43:32 +0700

    membuat post category dan memahami eloquent relationship

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,20 @@
+
+
+  @extends('layouts.main')
+
+@section('container')
+  <h1 class = "mb-5">Post Categories</h1>
+  
+@foreach ($Categories as $category)
+  <ul>
+    <li>
+    <h2><a href="/categories/{{ $category->slug }} ">{{ $category->name }}</a></h2>
+    </li>
+  </ul>
+
+  
+  @endforeach
+
+
+@endsection
+
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
   
     <h1 class="mb-5">{{ $post->title }}</h1>
+
+    <p>By. Chandra Dwi Ramadhani in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
   
 
